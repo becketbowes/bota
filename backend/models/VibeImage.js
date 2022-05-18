e3778feaf2d753cba07a321b110989413dddec98
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-18 16:19:44 -0500

    reformatted backend code

--- backend/models/VibeImage.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const { Schema } = mongoose;
-
-const VibeImage = new Schema({});
-
-const vibeImage = mongoose.model("vibeImage", vibeImageSchema);
-
-module.exports = VibeImage;
--- server/models/Index.js
+++ server/models/Index.js
@@ -2,6 +2,6 @@ const Inventory = require("./Inventory");
 const Invoice = require("./Invoice");
 const Product = require("./Product");
 const VibeImage = require("./VibeImage");
-const VIbeText = require("./VibeText");
+const VibeText = require("./VibeText");
 
 module.exports = { Inventory, Invoice, Product, VibeImage, VibeText };
