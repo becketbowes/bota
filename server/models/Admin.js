121a82f4b0ef5760579afa167b1e9951ed628f71
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-18 16:34:14 -0500

    create admin collection

---
+++ server/models/Admin.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const bcrypt = require("bcrypt")
+
+const adminSchema = new Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must match an email address!']
+        },
+        password: {
+            type: String,
+            require: true,
+            minlength: 10
+        }
+    }
+);
+
+// set up pre-save middleware to create password
+adminSchema.pre('save', async function(next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // compare the incoming password with the hashed password
+  adminSchema.methods.isCorrectPassword = async function(password) {
+    return bcrypt.compare(password, this.password);
+  };
+
+const Admin = model("Admin", adminSchema);
+
+module.exports = Admin
--- server/models/Index.js
+++ server/models/Index.js
@@ -1,7 +1,8 @@
+const Admin = require("./Admin");
 const Inventory = require("./Inventory");
 const Invoice = require("./Invoice");
 const Product = require("./Product");
 const VibeImage = require("./VibeImage");
 const VibeText = require("./VibeText");
 
-module.exports = { Inventory, Invoice, Product, VibeImage, VibeText };
+module.exports = { Admin, Inventory, Invoice, Product, VibeImage, VibeText };
