c76ad04c646ce598ecb7fb498731e9c058641829
Lisa Jean <MsJean@Lisas-Air.fios-router.home>
2022-05-19 19:50:43 -0400

    backend-note

--- backend/models/Inventory.js
+++ backend/models/Inventory.js
@@ -3,9 +3,18 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const invetorySchema = new Schema({
-
-
-    
+  sku: {
+    type: String,
+    require: true,
+  },
+  products: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  amount: {
+    type: Number,
+    default: 0,
+  },
 });
 
 const Inventory = mongoose.model("Inventory", inventorySchema);
