6beee29c242e49dbe9f55eb122b88063a3ca6369
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-19 19:41:33 -0500

    .

--- backend/models/Invoice.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const { Schema } = mongoose;
-
-const invoiceSchema = new Schema({});
-
-const Category = mongoose.model("Invoice", invoiceSchema);
-
-module.exports = Invoice;
--- server/models/Note.js
+++ server/models/Note.js
@@ -1,27 +1,35 @@
 const mongoose = require("mongoose");
+const dateFormat = require('../utils/dateFormat')
 
 const { Schema } = mongoose;
 
-const noteSchema = new Schema({
-  writerName: {
-    type: String,
-    require: True,
+const noteSchema = new Schema(
+  {
+    writerName: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Must match an email address!"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    read: {
+      type: Boolean,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.+@.+\..+/, "Must match an email address!"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (timestamp) => dateFormat(timestamp),
-  },
-  read: {
-    type: Boolean,
-  },
-});
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
 
 const Note = mongoose.model("Note", noteSchema);
 
