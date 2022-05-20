e306e06b225a177a841a5de949f9e764c612b6c6
ccd330 <claudiacdavis@gmail.com>
2022-05-19 20:37:19 -0400

    add sample faq

---
+++ src/components/FAQ/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+function FAQ () {
+
+    return (
+        <div className="faq">
+            <div className="faq-title">
+                <h1>Frequently Asked Questions</h1>
+            </div>
+            <div className="faq-section"> 
+                <h3>
+                    Sustainability
+                </h3>
+            </div>
+            <div className ="faq-question">
+                <h6>
+                    Where do you source your ingredients from?
+                </h6>
+            </div>
+            <div className="faq-answer">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                </p>
+            </div>
+            <div className="faq-section"> 
+                <h3>
+                    Shipping
+                </h3>
+            </div>
+            <div className ="faq-question">
+                <h6>
+                    Is there a way to track my order?
+                </h6>
+            </div>
+            <div className="faq-answer">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                </p>
+            </div>
+            <div className ="faq-question">
+                <h6>
+                    Do you offer refunds?
+                </h6>
+            </div>
+            <div className="faq-answer">
+                <p>
+                    Bota is not able to offer refunds at this time.
+                </p>
+            </div>
+        </div>
+    
+    )
+};
+
+export default FAQ;
