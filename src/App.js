dd7ef3dd63a7dea72cdba4fc9b548cf6af45304c
Catherine McNeel <catherinemcneel@icloud.com>
2022-05-20 11:14:30 -0400

    contact forn

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import React, { useState } from 'react';
-import Home from './components/home/index';
-import Product from './components/product/index';
+import react from 'react';
+
 import Head from './components/head/index';
-import Nav from './components/nav/index';
-import About from './components/about/index';
-import Shop from './components/shop/index';
-import Gift from './components/gift/index';
-import Contact from './components/contact/index';
+
 
 import FAQ from './components/FAQ/index';
 import Toe from './components/toe'
-
-
-
-
 
 function App() {
 
--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { useState} from 'react';
+import { validateEmail } from '../utils/helpers';
 
 function Contact () {
 
-    
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
+    const [errorMessage, setErrorMessage] = useState('');
+    const { name, email, message } = formState;
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (!errorMessage) {
+        console.log('Submit Form', formState);
+      }
+    };
+  
+    const handleChange = (e) => {
+      if (e.target.name === 'email') {
+        const isValid = validateEmail(e.target.value);
+        if (!isValid) {
+          setErrorMessage('Your email is invalid.');
+        } else {
+          setErrorMessage('');
+        }
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required.`);
+        } else {
+          setErrorMessage('');
+        }
+      }
+      if (!errorMessage) {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+        console.log('Handle Form', formState);
+      }
+    };
+  
     return (
-        <h1>Contact</h1>
-
-        //name, email, message
-    )
-
-};
-
+      <section className="contact-item">
+          <div className=''>
+        <div>
+            <h1 data-testid="h1tag">GET IN TOUCH</h1>
+        </div>
+        <form id="contact-form" onSubmit={handleSubmit}>
+          <div className='name-section'>
+            <label className='name' htmlFor="name">NAME</label>
+            <input className='input-box' type="text" name="name" defaultValue={name} onBlur={handleChange} />
+          </div>
+          <div>
+            <label className='name' htmlFor="email">EMAIL</label>
+            <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
+          </div>
+          <div>
+            <label className='name' htmlFor="message">MESSAGE</label>
+            <input name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+          </div>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
+            </div>
+          )}
+          <button data-testid="button" type="submit">SUBMIT</button>
+        </form>
+        </div>
+      </section>
+    );
+  }
 export default Contact;
