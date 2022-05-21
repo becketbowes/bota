df7162a81a339ef43b46391d797789a311ddd3fe
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-21 15:01:01 -0500

    .

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build"));
+  res.sendFile(path.join(__dirname, "../client/build"));
 });
 
 startApolloServer(typeDefs, resolvers);
