4c5ecab1988774d56cc4a7bd4828a3b783f12ae1
Becket <95153115+becketbowes@users.noreply.github.com>
2022-05-23 02:48:08 -0400

    frontend apollo and gql added successfully

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,27 @@
 import Head from './components/head/index';
 import Toe from './components/toe';
 import Admin from './components/admin';
-// import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-// import { setContext } from '@apollo/client/link/context';
 
-// const httpLink = createHttpLink({ uri: '/graphql' });
-
-// const authLink = setContext((_, { headers }) => {
-//   const toke = localStorage.getItem('id_token');
-//   return {
-//     headers: {
-//       ...headers, 
-//       authorization: toke ? `Bearer ${id_token}` : '',
-//     },
-//   };
-// });
 
 // const client = new ApolloClient({
-//   link: authLink.concat(httpLink),
+//   uri: 'https://http://localhost:3001//graphql',
+//   cache: new InMemoryCache()
+// });
+
+// client  
+//   .query({
+//     query: gql`
+//       query GetRates {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
+
+// const client = new ApolloClient({
+//   uri: 'https://48p1r2roz4.sse.codesandbox.io',
 //   cache: new InMemoryCache()
 // });
 
@@ -29,7 +33,6 @@ function App() {
       {/* <ApolloProvider client={client}> */}
           <Head></Head>
           <Admin></Admin>
-          {/* <Modify></Modify> */}
           <Toe></Toe>
       {/* </ApolloProvider> */}
       </>
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache()
+});
+
+// function App() {
+//   return (
+//     <div>
+//       <h2>Apollo App</h2>
+//     </div>
+//   );
+// }
+
+render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
