f1206c40442502651dfb9d66a0baef1cca91f7d7
ccd330 <claudiacdavis@gmail.com>
2022-05-25 19:40:31 -0400

    fixed link to contact page need to fix faq link

--- client/src/components/toe/index.js
+++ client/src/components/toe/index.js
@@ -1,14 +1,31 @@
 import { useState } from 'react'
 import dot from '../../assets/img/botadot.png';
 import Toenav from './toenav.js';
+import Contact from '../contact/index';
+import FAQ from '../FAQ/index';
+import Home from '../home/index';
 
 function Toe() {
     const [toenav, setToenav] = useState(false);
+    const [currentPage, setCurrentPage] = useState('Home');
 
     const toeMenu = () => {
         setTimeout(() => setToenav(false), 5000)
         setToenav(true);
     }
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    const renderPage = () => {
+        if (currentPage === 'FAQ') {
+            return <FAQ />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        return null;
+        };
+
     
     return (
         <footer>
@@ -16,7 +33,8 @@ function Toe() {
             <img src={dot} className="botadotdot" alt="a round round circle" onClick={toeMenu}/>
             </div>
             <div>
-                <Toenav toenav={toenav} />
+                <Toenav toenav={toenav} currentPage={currentPage} handlePageChange={handlePageChange} />
+                {renderPage()}
             </div>
             {/*<Toenav toenav={toenav} /> */}
         </footer>
--- client/src/components/toe/toenav.js
+++ client/src/components/toe/toenav.js
@@ -1,75 +1,43 @@
 import { useState } from 'react';
 import PrivacyModal from '../privacy/index';
+import Instagram from '../../assets/img/IG_icon.png';
 
-{/*function Toenav({ toenav }) {
-  if (!toenav) {
-    return null
-  }
-  return (
-      <div className='toenavmenu'>
-        <ul>
-          <li className="link">
-            <span href='#Contact' onClick={() => handlePageChange('Contact')}
-              className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>
-                Contact
-              </span>
-          </li>
-          <li>
-            <span href='#FAQ' onClick={() => handlePageChange('FAQ')}
-            className={currentPage === 'FAQ' ? 'nav-link active' : 'nav-link'}> 
-                FAQ
-            </span>
-          </li>
-          <li>
-            <span href='#Social' onClick={() => handlePageChange('Social')}
-            className={currentPage === 'Social' ? 'nav-link active' : 'nav-link'}> 
-                Social
-            </span>
-          </li>
-          <li>
-            <span href='#Privacy' onClick={() => handlePageChange('Privacy')}
-            className={currentPage === 'Privacy' ? 'nav-link active' : 'nav-link'}> 
-                Privacy Policy
-            </span>
-          </li>
-          <li>
-            <span href='https://github.com/becketbowes/bota'>
-              &copy; Group2Designs 
-            </span>
-          </li>
-        </ul>
-      </div>
-  );
-} */}
 
-function Toenav({ toenav }) {
+
+function Toenav({ currentPage, handlePageChange, toenav }) {
+
   const [openModal, setOpenModal] = useState(false)
+
+
   if (!toenav) {
     return null
   }
   return (
     <nav className="footer-nav">
       <li>
-        <a className="footer-link">Contact</a>
+        <a href='#Contact' onClick={() => handlePageChange('Contact')}
+        className={currentPage === 'Contact' ? 'footer-link active' : 'footer-link'}>
+          Contact</a>
       </li>
       <li>
-        <a className="footer-link">
+        <a href='#FAQ' onClick={() => handlePageChange('About')}
+        className={currentPage === 'FAQ' ? 'footer-link active' : 'footer-link'} >
           FAQ
         </a>
       </li>
       <li>
-        <a className="footer-link">
-          Social
+        <a href="https://www.instagram.com/igbosupply/">
+          <img src={Instagram} alt="instagram-logo" />
         </a>
       </li>
-      <li>
+      <li className='privacyModal'>
         <a className="footer-link openModal" onClick={() => {
           setOpenModal(true);
           }}>Privacy Policy</a>
         {openModal &&<PrivacyModal closeModal={setOpenModal} />}
       </li>
       <li>
-        <a href="https://github.com/becketbowes/bota" className="footer-link">
+        <a href="https://github.com/becketbowes/bota">
           &copy; Group2Designs 
         </a>
       </li>
