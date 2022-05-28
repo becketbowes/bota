4494603c5ef21a40ee16d89c563cd4b9a0f9f4a6
Becket <95153115+becketbowes@users.noreply.github.com>
2022-05-27 22:27:00 -0400

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:3333/bota",
+  process.env.MONGODB_URI || "mongodb://localhost:3333/bota"  || 'mongodb+srv://chinny:nVaQiTvFY2pgeIZd@cluster0.3hyqse3.mongodb.net/?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
