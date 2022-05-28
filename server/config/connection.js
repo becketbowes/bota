e8223791eb7eff3dfcf25464eede87f59494b38c
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-27 20:03:42 -0500

    Deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGDB_URI || "mongodb://127.0.0.1/Cluster0" || process.env.MONGODB_URI,
+  process.env.MONGODB_URI || "mongodb://localhost/mernshopping",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
