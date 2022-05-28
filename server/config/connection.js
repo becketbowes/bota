e7ebf0094c2a38776e8b38b57352385e8d98f85f
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-27 19:38:48 -0500

    Deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGDB_URI || "mongodb://localhost:27017/bota-databas?directConnection=true" || process.env.MONGODB_URI,
+  process.env.MONGDB_URI || "mongodb://127.0.0.1/mernshopping" || process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
