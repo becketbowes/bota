7cc9e7200ac96465cc26a92d327836de7746cbef
Mubo Houston <89697973+MuboHouston@users.noreply.github.com>
2022-05-27 19:45:19 -0500

    Deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGDB_URI || "mongodb://127.0.0.1/mernshopping" || process.env.MONGODB_URI,
+  process.env.MONGDB_URI || "mongodb://127.0.0.1/Cluster0" || process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // useCreateIndex: true,
+    useCreateIndex: true,
     // useFindAndModify: false,
   }
 );
